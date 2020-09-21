0f2fccbab814b04500748c579371979e55e60df5
mopratt <47038455+mopratt@users.noreply.github.com>
2020-09-21 16:29:12 -0500

    Add files via upload

--- rcode/fluviz-app/metamap/app.R
+++ rcode/fluviz-app/metamap/app.R
@@ -1,36 +1,45 @@
 library(shiny)
-ui <-  fluidPage(
-  tags$h1("fluviz"), 
-  tags$h4("Visualization of Influenza Sequences and Metadata"),
-  fluidRow(
-    column(6,
-           textInput(inputId = "tree", 
-            label = "Name of Tree Object:")),
-    column(6, 
-           textInput(inputId = "meta", 
-                     label = "Metadata Object:")),
-    ),
-  fluidRow(
-    column(2, offset = 5, 
-           actionButton(inputId = "submit", label = "Submit"))
-  ),
-  fluidRow(
-    column(10, 
-           checkboxGroupInput(inputId = "choose",
-                              label = "Choose Columns:", 
-                              choices = c("Clade", "Subtype", "Host"), 
-                              selected = "Clade", 
-                              inline = TRUE))
-  ),
-  fluidRow(
-    column(2, offset = 5,
-           actionButton(inputId = "go", label = "Plot"))
-  ),
-  fluidRow(
-    column(12, 
-           plotOutput("metamap"))
+ui <-  navbarPage(title = "fluviz", 
+             tabPanel(title = "Introduction", 
+                      tags$h1("fluviz"), 
+                      tags$h4("Visualization of Influenza Sequences and Metadata")),
+             tabPanel(title = "combine metadata"),
+             tabPanel(title = "plot metadata heatmap",
+                      sidebarLayout(
+                        sidebarPanel(
+                          wellPanel(fluidRow(
+                            column(6,
+                                   textInput(inputId = "tree", 
+                                             label = "Name of Tree Object in R:"),
+                                   textInput(inputId = "meta", 
+                                             label = "Name of Metadata Object in R:"))
+                          ),
+                          fluidRow(
+                            column(2, 
+                                   actionButton(inputId = "submit", label = "Submit"))
+                          )),
+                          wellPanel(fluidRow(
+                            column(10, 
+                                   checkboxGroupInput(inputId = "choose",
+                                                      label = "Choose Columns (click Submit to update):", 
+                                                      choices = c("Clade", "Subtype", "Host"), 
+                                                      selected = "Clade", 
+                                                      inline = TRUE))
+                          ),
+                          fluidRow(
+                            column(2,
+                                   actionButton(inputId = "go", label = "Plot"))
+                          ))),
+                        mainPanel(
+                          fluidRow(
+                            column(12, 
+                                   plotOutput("metamap"))
+                            )
+                          )
+                        )
+                      ),
+             tabPanel(title = "interactive snpplot")
   )
-)
 server <- function(input, output, session) {
   library(ggplot2)
   library(tidyverse)
@@ -49,7 +58,7 @@ server <- function(input, output, session) {
   })
   observeEvent(input$submit, {
     updateCheckboxGroupInput(session, "choose", 
-                             label = "Choose Columns:", 
+                             label = "Choose Columns from the Data:", 
                              choices = cols(), 
                              selected = "Clade", 
                              inline = TRUE)
@@ -61,7 +70,7 @@ server <- function(input, output, session) {
     met <- metadata %>% select(cols) %>% data.frame()
     treet <- tree %>% as.phylo() %>% as_tibble()
     treet_data <- treet %>% left_join(metadata, by = 'label')
-    tree <- as.treedata(treet_data) %>% ggtree(branch.length = "none") + geom_tiplab(size = 2)
+    tree <- as.treedata(treet_data) %>% ggtree(branch.length = "none") + geom_tiplab(size = 4)
     rownames(met) <- metadata$label # metadata file must have first column as labels
     low = "red"
     high = "green"
@@ -78,14 +87,14 @@ server <- function(input, output, session) {
                                           offset = ofs, width = 0.1, 
                                           colnames_position = "top", 
                                           colnames_offset_y = 1, 
-                                          font.size = 3) +
+                                          font.size = 4) +
                    scale_fill_gradient(low = low, high = high, na.value = NA, name = i))
         } else {
           assign(paste0("h", h), gheatmap(tree, get(paste0(i, ".df")),
                                           offset = ofs, width = 0.1, 
                                           colnames_position = "top", 
                                           colnames_offset_y = 1, 
-                                          font.size = 3) +
+                                          font.size = 4) +
                    scale_fill_viridis_d(option = LETTERS[let], name = i))
         }
       } else {
@@ -100,7 +109,7 @@ server <- function(input, output, session) {
                                           offset = ofs, width = 0.1,
                                           colnames_position = "top", 
                                           colnames_offset_y = 1, 
-                                          font.size = 3) +
+                                          font.size = 4) +
                    scale_fill_gradient(low = low, high = high, na.value = NA, name = i))
         }
         else {
@@ -108,19 +117,18 @@ server <- function(input, output, session) {
                                           offset = ofs, width = 0.1,
                                           colnames_position = "top", 
                                           colnames_offset_y = 1, 
-                                          font.size = 3) +
+                                          font.size = 4) +
                    scale_fill_viridis_d(option = LETTERS[let], name = i))
         }
       }
     }
-    get(paste0("h", h)) + theme(legend.position = "bottom") + coord_cartesian(clip = "off")
+    get(paste0("h", h)) + theme(legend.position = "right", legend.box = "vertical")
   }
   map <- eventReactive(input$go, {
     metamap(tree = tree(), metadata = metadata(), 
-            cols = choices()) +
-      theme(legend.position = "right")
+            cols = choices())
   })
-  output$metamap <- renderPlot({map()})
+  output$metamap <- renderPlot({map()}, width = 1500, height = 1000)
   }
 
 shinyApp(ui = ui, server = server)
